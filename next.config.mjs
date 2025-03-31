38719442ff03d2f3b4672b064cdf9bd3a3a62ab7
Bobby Martin <therealdjbobbydrake@gmail.com>
2025-03-31 09:23:06 -0400

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,9 @@
 const nextConfig = {};
 
 export default nextConfig;
+module.exports = {
+    env: {
+      NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    },
+  };
+  
